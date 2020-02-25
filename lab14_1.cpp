69a03fb4685bd6b53d68acedc119eb2f793b8365
phird <iamphird@gmail.com>
2020-02-25 17:21:09 +0700

    25/02/2020

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,69 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 
 int main(){
 	//Write your code here.
+	int a=5;
+	char b='A';
+	char &c = b;
+	int *x= &a ; 
+	char *y = &b ;
+	int**z= &x;
 	
 	
+	cout << "a = " << a  << endl;
+	cout << "b = " << &b  << endl;
+	cout << "c = " << c  << endl;
+	cout << "*x = " << x << endl;
+	cout << "*y = " << (void *)y << endl;
+	cout << "**z = " << z << endl; 
+	cout << "======================================================"<< endl;
+	// show address
+	cout << "Address a = " << &a << endl;
+	cout << "Address b = " << (void *)&b << endl;
+	cout << "Address c = " << (void *)&c << endl;
+	cout << "Address x = " << &x << endl;
+	cout << "Address y = " << &y << endl;
+	cout << "Address z = " << &z << endl;
+	cout << "======================================================"<< endl;
+	//change c to F 
+	c='F';
+	cout << "a = " << a  << endl;
+	cout << "b = " << &b  << endl;
+	cout << "c = " << c  << endl;
+	cout << "*x = " << x << endl;
+	cout << "*y = " << (void *)y << endl;
+	cout << "**z = " << z << endl; 
+	cout << "======================================================"<< endl;
+	//change y to W by pointer
+	*y= 'W';
+	cout << "a = " << a  << endl;
+	cout << "b = " << &b  << endl;
+	cout << "c = " << c  << endl;
+	cout << "*x = " << x << endl;
+	cout << "*y = " << (void *)y << endl;
+	cout << "**z = " << z << endl; 
+
+	cout << "======================================================"<< endl;
+	//change a to 6 by pointer
+	*x= 6 ;
+	cout << "a = " << a  << endl;
+	cout << "b = " << &b  << endl;
+	cout << "c = " << c  << endl;
+	cout << "*x = " << x << endl;
+	cout << "*y = " << (void *)y << endl;
+	cout << "**z = " << z << endl; 
+	cout << "======================================================"<< endl;
 	
+	//change a to 7 by pointer
+	**z= 7;
+	cout << "a = " << a  << endl;
+	cout << "b = " << &b  << endl;
+	cout << "c = " << c  << endl;
+	cout << "*x = " << x << endl;
+	cout << "*y = " << (void *)y << endl;
+	cout << "**z = " << z << endl; 
+
 	return 0;
 }
--- lab14_4.cpp
+++ lab14_4.cpp
@@ -8,7 +8,7 @@ void shuffle(int &,int &, int &, int &);
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
 	
-	srand(time(0));	
+	//srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
 	    shuffle(a,b,c,d);
@@ -19,3 +19,39 @@ int main(){
 }
 
 //Write definition of shuffle() here 
+void shuffle(int &a,int &b,int &c,int &d)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		int temp=rand()%6+1;
+		switch (temp)
+		{
+		case 1:
+			swap(a,b);
+			break;
+		case 2:
+			swap(a,c);
+			break;
+		case 3:
+			swap(a,d);
+			break;	
+		case 4:
+			swap(b,c);
+			break;
+		case 5:
+			swap(b,d);
+			break;
+		case 6:
+			swap(c,d);
+			break;																													
+		}
+	}
+}
+void swap(int &x, int &y)
+{
+	int temp=x;
+	x=y;
+	y=temp;
+}
+
+
